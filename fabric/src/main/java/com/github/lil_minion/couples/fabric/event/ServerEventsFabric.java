c74e142d0e0b5d81c490539777bdc54c7f7a6466
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-23 21:03:16 +0100

    [FEATURE] add event listeners for server started

---
+++ fabric/src/main/java/com/github/lil_minion/couples/fabric/event/ServerEventsFabric.java
@@ -0,0 +1,12 @@
+package com.github.lil_minion.couples.fabric.event;
+
+import com.github.lil_minion.handler.ServerEventHandler;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
+public class ServerEventsFabric {
+
+    public static void register() {
+        ServerLifecycleEvents.SERVER_STARTED.register(ServerEventHandler::onServerStarted);
+    }
+
+}
---
+++ neoforge/src/main/java/com/github/lil_minion/couples/neoforge/event/ServerEventsNeoForge.java
@@ -0,0 +1,17 @@
+package com.github.lil_minion.couples.neoforge.event;
+
+import com.github.lil_minion.Couples;
+import com.github.lil_minion.handler.ServerEventHandler;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
+
+@EventBusSubscriber(modid = Couples.MOD_ID)
+public class ServerEventsNeoForge {
+
+    @SubscribeEvent
+    private static void onServerStarted(ServerStartedEvent event) {
+        ServerEventHandler.onServerStarted(event.getServer());
+    }
+
+}
