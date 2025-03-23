6adb9271ac80780b17d1e05f05c1e9d80699bb27
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-23 21:08:30 +0100

    [FEATURE] create inbox data class and register with server event handler

---
+++ common/src/main/java/com/github/lil_minion/server/data/InboxData.java
@@ -0,0 +1,106 @@
+package com.github.lil_minion.server.data;
+
+import com.github.lil_minion.Couples;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.datafix.DataFixTypes;
+import net.minecraft.world.level.saveddata.SavedData;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+import java.util.List;
+
+public class InboxData extends SavedData {
+
+    public static final Map<UUID, Inbox> PLAYER_INBOX_MAP = new HashMap<>();
+
+    public static SavedData INSTANCE;
+
+    public static void createServerState(MinecraftServer server) {
+        INSTANCE = server.overworld().getDataStorage().computeIfAbsent(
+                new Factory<>(InboxData::create, InboxData::load,
+                        DataFixTypes.LEVEL), Couples.MOD_ID + ".inbox_data");
+        INSTANCE.setDirty();
+    }
+
+    public static InboxData create() {
+        return new InboxData();
+    }
+
+    public static InboxData load(CompoundTag tag, HolderLookup.Provider registryLookup) {
+        PLAYER_INBOX_MAP.clear();
+        CompoundTag compoundTag = tag.getCompound("couples.mail_data");
+
+        for (String key : compoundTag.getAllKeys()) {
+            CompoundTag inboxTag = compoundTag.getCompound(key);
+
+            UUID playerUUID = inboxTag.getUUID("player");
+
+            CompoundTag mailListTag = inboxTag.getCompound("mails");
+            List<Mail> mailList= new ArrayList<>();
+
+            Inbox inbox = new Inbox(playerUUID);
+            for (String mailKey : mailListTag.getAllKeys()) {
+                CompoundTag mailTag = mailListTag.getCompound(mailKey);
+                UUID sender = mailTag.getUUID("sender");
+                UUID recipient = mailTag.getUUID("recipient");
+                MailType type = MailType.fromString(mailTag.getString("type"));
+                Long timestamp = mailTag.getLong("timestamp");
+
+                CompoundTag pagesTag = mailTag.getCompound("pages");
+                List<Component> message = new ArrayList<>();
+                for (String page : pagesTag.getAllKeys()) {
+                    message.add(Component.literal(page));
+                }
+
+                Mail mail = new Mail(sender, recipient, type, timestamp, message);
+                inbox.getMails().add(mail);
+            }
+            PLAYER_INBOX_MAP.put(playerUUID, inbox);
+        }
+
+        return new InboxData();
+    }
+
+    @Override
+    public @NotNull CompoundTag save(CompoundTag tag, HolderLookup.Provider registries) {
+        CompoundTag compoundTag = new CompoundTag();
+
+        for (Inbox inbox : PLAYER_INBOX_MAP.values()) {
+            CompoundTag inboxTag = new CompoundTag();
+            inboxTag.putUUID("player", inbox.getPlayerUUID());
+
+            CompoundTag mailsTag = new CompoundTag();
+            int i = 0;
+            for (Mail mail : inbox.getMails()) {
+                CompoundTag mailTag = new CompoundTag();
+                mailTag.putUUID("sender", mail.sender());
+                mailTag.putUUID("recipient", mail.recipient());
+                mailTag.putString("type", mail.type().getType());
+                mailTag.putLong("timestamp", mail.timestamp());
+
+                // Add message
+                CompoundTag pagesTag = new CompoundTag();
+                int j = 0;
+                for (Component component : mail.message()) {
+                    pagesTag.putString( "" + j, component.getString());
+                    j++;
+                }
+
+                mailTag.put("pages", pagesTag);
+
+                mailsTag.put("" + i, mailTag);
+                i++;
+            }
+            inboxTag.put("mails", mailsTag);
+
+            compoundTag.put(inbox.getPlayerUUID().toString(), inboxTag);
+        }
+
+        tag.put("couples.mail_data", compoundTag);
+        return tag;
+    }
+
+}
