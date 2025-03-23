bbdc0d073a7dc7bbf18f7af259fd1948598cabb2
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-23 21:02:39 +0100

    [FIX] useOn() should be use() with environment -> client only

--- common/src/main/java/com/github/lil_minion/item/WeddingRingItem.java
+++ common/src/main/java/com/github/lil_minion/item/WeddingRingItem.java
@@ -1,8 +1,14 @@
 package com.github.lil_minion.item;
 
-import net.minecraft.world.InteractionResult;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
 import org.jetbrains.annotations.NotNull;
 
 public class WeddingRingItem extends Item {
@@ -11,7 +17,8 @@ public class WeddingRingItem extends Item {
     }
 
     @Override
-    public @NotNull InteractionResult useOn(UseOnContext context) {
-        return super.useOn(context);
+    @Environment(EnvType.CLIENT)
+    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        return super.use(level, player, usedHand);
     }
 }
